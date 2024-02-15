3d7cee1eeb8f9262a29d995bec9c9bcacb345514
iBobbyTS <ibobbyts@gmail.com>
2024-02-14 17:55:52 -0700

    implement operations

--- matrix.js
+++ matrix.js
@@ -53,6 +53,27 @@ const showResult = (title, containerId, rows, cols, dataArray) => {
 const showResult2D = (title, containerId, dataArray) => {
 	// dataArray is a 2D array
 	// complete this function based on the showResult function
+
+}
+
+function checkMatricesDimForAddSub(matrix1, matrix2){
+    if (matrix1.length !== matrix2.length) {
+        alert(`Row count doesn't match: \nMatrix 1: ${matrix1.length}\nMatrix 2: ${matrix2.length}`)
+        return 1
+    }
+    if (matrix1[0].length !== matrix2[0].length) {
+        alert(`Column count doesn't match: \nMatrix 1: ${matrix1[0].length}\nMatrix 2: ${matrix2[0].length}`)
+        return 1
+    }
+    return 0
+}
+
+function checkMatricesDimForMul(matrix1, matrix2){
+    if (matrix1[0].length !== matrix2.length) {
+        alert(`Dimension doesn't match: \nMatrix 1 columns: ${matrix1[0].length}\nMatrix 2 rows: ${matrix2.length}`)
+        return 1
+    }
+    return 0
 }
 
 function performOperation(operation) {
@@ -62,11 +83,37 @@ function performOperation(operation) {
     console.log("2nd Matrix", matrix2);
     console.log("Operation", operation);
     // Just a test result
-    let result = [1, 2, 3, 4, 5, 6, 7, 8];
+    let result = [];
+    let rows = 0;
+    let cols = 0;
+    if (operation === 'add') {
+        if (checkMatricesDimForAddSub(matrix1, matrix2)){
+            return
+        }
+        result = addMatrices(matrix1, matrix2)
+        rows = matrix1.length
+        cols = matrix1[0].length
+    } else if (operation === 'subtract') {
+        if (checkMatricesDimForAddSub(matrix1, matrix2)){
+            return
+        }
+        result = subtractMatrices(matrix1, matrix2)
+        rows = matrix1.length
+        cols = matrix1[0].length
+    } else if (operation === 'multiply') {
+        if (checkMatricesDimForMul(matrix1, matrix2)) {
+            return
+        }
+        result = multiplyMatrices(matrix1, matrix2)
+        rows = matrix1.length
+        cols = matrix2[0].length
+    } else {
+        alert(`Operation ${operation} not allowed`)
+    }
     // Call your matrix calculation functions here
     // For example: if (operation === 'add') { addMatrices(matrix1, matrix2); }
 	// prints suitable messages for impossible situation
-    showResult('The Result', 'matrix3', 2, 4, result); // use suitable function for printing results
+    showResult('The Result', 'matrix3', rows, cols, result); // use suitable function for printing results
 }
 
 const getMatrixData1D = function (matrixId) {
@@ -103,12 +150,40 @@ const getMatrixData2D = function (matrixId) {
 
 // Add your matrix calculation functions here
 // The functions must check the posibility of calculation too.
-function addMatrices(matrix1, matrix2){ 
-	// provide the code
+function addMatrices(matrix1, matrix2){
+    let matrix3 = []
+    for (let i=0; i<matrix1.length; i++) {
+        let row = [];
+        for (let j=0; j<matrix1[0].length; j++) {
+            row.push(matrix1[i][j]+matrix2[i][j])
+        }
+        matrix3.push(row)
+    }
+    return matrix3;
 }
-const subtractMatrices = function (matrix1, matrix2) { 
-	// provide the code
+const subtractMatrices = function (matrix1, matrix2) {
+    let matrix3 = []
+    for (let i=0; i<matrix1.length; i++) {
+        let row = [];
+        for (let j=0; j<matrix1[0].length; j++) {
+            row.push(matrix1[i][j]-matrix2[i][j])
+        }
+        matrix3.push(row)
+    }
+    return matrix3;
 };
-const multiplyMatrices = (matrix1, matrix2) => { 
-	// provide the code
+const multiplyMatrices = (matrix1, matrix2) => {
+    let matrix3 = []
+    for (let i=0; i<matrix1.length; i++) {
+        let row = [];
+        for (let j=0; j<matrix2[0].length; j++) {
+            let x = 0;
+            for (let k=0; k<matrix2.length; k++){
+                x += matrix1[i][k]*matrix2[k][j]
+            }
+            row.push(x)
+        }
+        matrix3.push(row)
+    }
+    return matrix3;
 };
